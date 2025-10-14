91640719cf9510b32425a09574b068f560935833
binboupan <nullcollision@kagura.eu>
2025-10-15 00:17:05 +0300

    Fix extra whitespaces

--- .github/README.md
+++ .github/README.md
@@ -3,7 +3,7 @@
 
 ## About this module
   
-This is a simple module which adds a command ".daily reset" which resets all the current instances for the player. I made this module because I prefer to have a simple command instead of an NPC to do this.  
+This is a simple AzerothCore module which adds the command ".daily reset" which resets all the current instances for the player. I made this module because I prefer to have a simple command instead of an NPC to do this.  
   
 ## Configuration  
   
--- src/DailyReset.cpp
+++ src/DailyReset.cpp
@@ -8,8 +8,6 @@
 
 using namespace Acore::ChatCommands;
 
-
-// Add player scripts
 class DailyReset : public PlayerScript
 {
 public:
@@ -17,47 +15,40 @@ public:
 
     void OnPlayerLogin(Player* player) override
     {
-            ChatHandler(player->GetSession()).PSendSysMessage("Type .daily reset to reset your daily dungeon limits.");
+        ChatHandler(player->GetSession()).PSendSysMessage("Type .daily reset to reset your daily dungeon limits.");
     }
 };
 
-// Chat command script for .reset daily
 class DailyResetCommandScript : public CommandScript
 {
 public:
     DailyResetCommandScript() : CommandScript("DailyResetCommandScript") { }
 
-  ChatCommandTable GetCommands() const override
+    ChatCommandTable GetCommands() const override
     {
         static ChatCommandTable gcCommandTable =
         {
-            { "reset",  HandleResetDailyCommand,  SEC_PLAYER, Console::Yes },
+            { "reset", HandleResetDailyCommand, SEC_PLAYER, Console::Yes },
         };
         static ChatCommandTable HandleGlobalChatCommandTable =
         {
             { "daily", gcCommandTable }
         };
         return HandleGlobalChatCommandTable;
-
     }
-    
 
     static bool HandleResetDailyCommand(ChatHandler* handler)
     {
-    Player* player = handler->GetSession()->GetPlayer();
-    if (!player)
-        return false;
+        Player* player = handler->GetSession()->GetPlayer();
+        if (!player)
+            return false;
 
-    // Reset daily dungeon limits
-    player->ResetDailyQuestStatus();
-    
-    // Send confirmation message
-    handler->PSendSysMessage("Your daily dungeon limits have been reset.");
-    return true;
+        player->ResetDailyQuestStatus();
+        handler->PSendSysMessage("Your daily dungeon limits have been reset.");
+        return true;
     }
 };
 
-// Add all scripts in one
 void AddDailyResetScripts()
 {
     new DailyReset();
